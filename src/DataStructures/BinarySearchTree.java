bac325cc0d340d584e0dc1ff171013204aa63b08
clnsih001 <clnsih001@cs.uct.ac.za>
2020-09-19 02:54:45 +0200

    Included the references Taks had added to the master branch

--- src/DataStructures/BinarySearchTree.java
+++ src/DataStructures/BinarySearchTree.java
@@ -1,3 +1,8 @@
+//references for the bst 
+//hussain for the interface
+//https://www.softwaretestinghelp.com/binary-search-tree-in-cpp/
+//https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+
 package DataStructures;
 
 public class BinarySearchTree<dataType extends Comparable<? super dataType>> extends BinaryTree<dataType>
